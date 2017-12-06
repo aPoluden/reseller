3eacc1b7ef77134872f73d6d80028c08d9250274
apoluden <artiomnkkm@gmail.com>
2017-12-06 16:00:40 +0200

    models creation: Advertisemen, Car, Seller

---
+++ scraper/classes/models.py
@@ -0,0 +1,27 @@
+import datetime
+
+class Advertisement:
+    
+    # Advertisement attributes 
+    id, name, url, comments = None, None, None, None
+    
+    def __init__(self):
+        # UTC timestamp when Advertisement instance was created
+        # FIXME: This solution is not accurate
+        created_at = datetime.datetime.utcnow()
+
+class Car:
+    
+    # Car attibutes
+    number_plate, attributes = None, {}
+
+    def __init__(self): 
+        pass
+
+class Seller: 
+    
+    # Seller attributes
+    phone_numbers = []
+    
+    def __init__(self):
+        pass 
--- scraper/tests/test.py
+++ scraper/tests/test.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
+import datetime
 
 from scraper.scraper import Scraper
 from scraper.classes.options import AdvertOptions
 from scraper.classes.autopscrapers import AutoPScraper
+from scraper.classes.models import Advertisement
 
 class ScraperTest(TestCase):
 
@@ -27,5 +29,16 @@ class AutoPScraperTest(TestCase):
         '''
         self.scraper.set_autop(AdvertOptions.CARS)
         # TODO FIX IT 
-        url = 'file:///home/apoluden/Programming/workspace/reseller/scraper/bmw_advertisement.html'
+        url = 'file:///home/apoluden/Programming/workspace/reseller/scraper/tests/bmw_advertisement.html'
         scraped_advert = self.scraper.scrape_particular_advert(url)
+
+class AdvertisementTest(TestCase):
+    
+    def test_created_at_field_assignement(self):
+        '''
+        Test created_at field UTC timestamp field assignement
+        on Advertisement instance creation
+        '''
+        advert = Advertisement()
+        self.assertEquals(datetime.datetime, type(advert.created_at))
+        
